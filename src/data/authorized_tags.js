6595a89ca9de7f40caed6ef286ca3b1ebe5cd6e9
kelmer1090 <psxkf3@nottingham.ac.uk>
2025-05-16 12:43:13 +0100

    Update authorized_tags.js

--- src/data/authorized_tags.js
+++ src/data/authorized_tags.js
@@ -1,5 +1,20 @@
-const userDefinedAuthorizedTags = {
-	"type:system": {
-		"description": "paper describing a system"
-	}
-}
+export default [
+  // years
+  "2018", "2019", "2021", "2023", "2024",
+
+  // themes
+  "regulatory_foundations",
+  "transparency_explainability",
+  "bias_fairness_controls",
+  "redress_accountability",
+
+  // methodologies
+  "qualitative",
+  "quantitative",
+
+  // evaluations
+  "none",
+  "case_study",
+  "user_study",
+  "xai_framework"
+];
